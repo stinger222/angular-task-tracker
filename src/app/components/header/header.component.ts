32821f7197773ce60c3e97d51a501e7064019997
= <azazasdfg.com@gmail.com>
2022-08-12 19:06:13 +0300

    Add styles for header and related

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'task-tracker'
+  title: string = 'Task Tracker'
 
   constructor() { }
 
