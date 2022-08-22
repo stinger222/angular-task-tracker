dc403408438cb65ad3cd2696b37c856fdf83c45e
= <azazasdfg.com@gmail.com>
2022-08-23 00:12:18 +0300

    Add some coditional rendering

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
-import { TaskService } from './../../services/task.service';
 import { UiService } from './../../services/ui.service';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,17 +13,19 @@ export class HeaderComponent {
   taskCreatorVisible: boolean
   taskCreatorSubscription: Subscription
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.taskCreatorSubscription = this.uiService.getTaskCreatorSubject().subscribe({
       next: (visibility) => {
         this.taskCreatorVisible = visibility
-        console.log('visibility: ', visibility);
-
       }
     })
   }
 
   toggleTaskCreator() {
     this.uiService.toggleTaskCreator()
+  }
+
+  matchesRoute(route: string) {
+    return this.router.url === route
   }
 }
